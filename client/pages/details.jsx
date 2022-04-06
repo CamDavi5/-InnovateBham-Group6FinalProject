032b26b8148c4f17b7e9c71ceb191c6f74115032
NHarris02 <Nharris091402@gmail.com>
2022-04-06 11:06:35 -0500

    routes'

---
+++ client/pages/details.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
+
+const Details = () => {
+    return (
+        <h1>Details page</h1>
+    )
+};
+
+export default Details;
