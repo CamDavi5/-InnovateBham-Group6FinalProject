9cdb401ebac9ab1f1f16bd2c34731618112de3b8
tpharris2488 <tpharris241988@gmail.com>
2022-04-10 13:53:14 -0500

    changes 4/10

--- client/pages/Categories.jsx
+++ client/pages/Categories.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-
-const Categories = () => {
+export default function Categories() {
     return (
-        <>
-            <div className="container text-center">
-                <div className="row">
-                    Categories Page
-                </div>
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Categories</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              Cars
+              <img src="./car-1.jpg" className="image"></img>
             </div>
-        </>
+            <div className="col-sm-12 col-md-6">
+              Household appliances
+              <img src="./car-2.jpg" className="image"></img>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <img src="./car-3.jpg" className="image"></img>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              Column 4
+              <img src="./car-4.jpg" className="image"></img>
+            </div>
+          </div>
+        </div>
+      </main>
     );
-}
-
-export default Categories;
+  }
