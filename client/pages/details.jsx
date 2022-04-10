4d79b84079719bfbf78a521fc26538dd58d0c23c
CamDavi5 <camrdavis.biz@gmail.com>
2022-04-10 14:43:30 -0500

    fix

--- client/pages/details.jsx
+++ client/pages/details.jsx
@@ -1,57 +1,15 @@
 import React from 'react';
 
-const Details = () => {
+const ItemsDetails = () => {
     return (
         <>
-
-
-
-            <div className="container test d-flex justify-content-center align-items-center flex-column push-to-bottom">
-                <div>
-                    <div id='detailCard' className="jumbotron">
-                        <h1 className="display-4">Lorem Ipsum</h1>
-                        <h1>test</h1>
-                        <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quis facilis error ipsam placeat labore, aperiam quia.</p>
-                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                        <p className="lead">
-
-                        </p>
-                    </div>
-
+            <div className="container text-center">
+                <div className="row">
+                    Items Details Page
                 </div>
-            </div>
-            <div className="container2 test d-flex justify-content-center align-items-center flex-column ">
-                <div className="jumbotron" id="detailCard3">
-                    <h1 className="display-4">Lorem ipsum</h1>
-                    <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quis facilis error ipsam placeat labore, aperiam quia.</p>
-                    <p>It uses utility namees for typography and spacing to space content out within the larger container.</p>
-                    <p className="lead" />
-
-                </div>
-            </div>
-            <div className="container3 test d-flex justify-content-center align-items-center flex-column position-">
-                <div className="jumbotron" id="detailCard2">
-                    <h1 className="display-4">Lorem ipsum</h1>
-                    <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quis facilis error ipsam placeat labore, aperiam quia.</p>
-                    <p>It uses utility namees for typography and spacing to space content out within the larger container.</p>
-                    <p className="lead" />
-                </div>
-            </div>
-            <div className="container4 test d-flex justify-content-center align-items-center flex-column position-">
-                <div className="jumbotron" id="detailCard4">
-                    <h1 className="display-4">Lorem ipsum</h1>
-                    <p className="lead">Lorem ipsum</p>
-                    <p></p>
-                    <p className="lead" />
-                </div>
-            </div>
-            <div class='product'>
-                <h5>Component</h5>
-                <h5>00/00/00</h5>
             </div>
         </>
-
     );
 }
 
-export default Details;
+export default ItemsDetails;
